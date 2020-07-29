eb900095838b50504a205a221632b981251b63c8
nikolozi2001 <34250988+nikolozi2001@users.noreply.github.com>
2020-07-29 14:50:56 +0400

    Add files via upload

---
+++ ROUGH.py
@@ -0,0 +1,7 @@
+thislist = ["0", "BMW X6 haman", "Porshe panamera", "BMW M3 E92", "BMW M5 E60", "Mercedes AMG e63s", "PRIUS", "CLS 55 AMG", "AUDI TT RS", "SUBARU BRZ", "FORD MUSTANG GT", "SUPRA", "LC 200", "FORD RAPTOR", "Lexus ISF", "BMW X5 E53", "G37 vs 350Z vs Traxxas", "Mcedes CL63 AMG", "BMW M3 vs C32 AMG", "C32 AMG", "Subaru Impreza WRX STI", "Nissan Leaf vs Chevi VOLT", "EVO X", "TESLA Model S", "BMW Z4", "G63 AMG Geländewagen", "Chevy Camaro SS", "Range Rover Sport SVR", "Toyota Altezza", "Chevy Corvette SuperCharger", "Lexus CT200h", "Nissan JUKE", "Mazda RX7", "Toyota 2019", "BMW X5 G05", "CLS 63 AMG", "Fiat 500 Abarth", "Chevrolet Volt", "Toyota C-HR", "BMW M5 F10", "Subaru Forester", "AUDI A7 - S Line", "BMW X5M F85", "Hundai Veloster", "Ford Fusion", "BMW M6 Grand Coupe", "Mercedes GLS 63 AMG", "KIA Optima LPG", "BMW M4", "GMC Sierra", "AUDI RS5", "Hundai Genesis Coupe Turbo", "Mercedes E55 AMG VS BMW M5", "BMW e34", "Mercedes W124 Coupe", "Toyota FJ Cruiser", "Jaguar E-pace", "BMW 330d", "Toyota LC Prado", "VW Golf R - Turbo", "BMW M5 e39", "Nissan 350Z", "Mercedes E63 AMG", "MINI Cooper S -vs- Fiat 500 Abarth", "Porsche 911 Carrera S", "BMW 135i", "Mercedes CLA 250"]
+
+print(thislist[6])
+
+# ეს არის ჩემი (ნიკოლოზ ქაჩიბაია) დაწერილი კოდი.
+
+# აქ ჩამოწერილია ყველა მანქანა, რომელიც გატესტა გიორგი სარიშვილმა!                                     (P.S რა თქმა უნდა მიმდევრობა დაცულია)
---
+++ ROUGH2.py
@@ -0,0 +1,33 @@
+cars = "BMW X6 haman", "Porshe panamera", "BMW M3 E92", "BMW M5 E60", "Mercedes AMG e63s", "PRIUS", "CLS 55 AMG", "AUDI TT RS", "SUBARU BRZ", "FORD MUSTANG GT", "SUPRA", "LC 200", "FORD RAPTOR", "Lexus ISF", "BMW X5 E53", "G37 vs 350Z vs Traxxas", "Mcedes CL63 AMG", "BMW M3 vs C32 AMG", "C32 AMG", "Subaru Impreza WRX STI", "Nissan Leaf vs Chevi VOLT", "EVO X", "TESLA Model S", "BMW Z4", "G63 AMG Geländewagen", "Chevy Camaro SS", "Range Rover Sport SVR", "Toyota Altezza", "Chevy Corvette SuperCharger", "Lexus CT200h", "Nissan JUKE", "Mazda RX7", "Toyota 2019", "BMW X5 G05", "CLS 63 AMG", "Fiat 500 Abarth", "Chevrolet Volt", "Toyota C-HR", "BMW M5 F10", "Subaru Forester", "AUDI A7 - S Line", "BMW X5M F85", "Hundai Veloster", "Ford Fusion", "BMW M6 Grand Coupe", "Mercedes GLS 63 AMG", "KIA Optima LPG", "BMW M4", "GMC Sierra", "AUDI RS5", "Hundai Genesis Coupe Turbo", "Mercedes E55 AMG VS BMW M5", "BMW e34", "Mercedes W124 Coupe", "Toyota FJ Cruiser", "Jaguar E-pace", "BMW 330d", "Toyota LC Prado", "VW Golf R - Turbo", "BMW M5 e39", "Nissan 350Z", "Mercedes E63 AMG", "MINI Cooper S -vs- Fiat 500 Abarth", "Porsche 911 Carrera S", "BMW 135i", "Mercedes CLA 250"
+marka = input("შეიყვანეთ მანქანა: ")
+nomrebi = "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"
+if marka == "BMW X6 haman":
+    print(nomrebi[0])
+elif marka == "Porshe panamera":
+    print(nomrebi[1])
+elif marka == "BMW M3 E92":
+    print(nomrebi[2])
+elif marka == "BMW M5 E60":
+    print(nomrebi[3])
+elif marka == "Mercedes AMG e63s":
+    print(nomrebi[4])
+elif marka == "PRIUS":
+    print(nomrebi[5])
+elif marka == "CLS 55 AMG":
+    print(nomrebi[6])
+elif marka == "AUDI TT RS":
+    print(nomrebi[7])
+elif marka == "SUBARU BRZ":
+    print(nomrebi[8])
+elif marka == "FORD MUSTANG GT":
+    print(nomrebi[9])
+elif marka == "SUPRA":
+    print(nomrebi[10])
+elif marka == "LC 200":
+    print(nomrebi[11])
+elif marka == "FORD RAPTOR":
+    print(nomrebi[12])
+elif marka == "Lexus ISF":
+    print(nomrebi[13])
+else:
+    print("შეცდომა")
